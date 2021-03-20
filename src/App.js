4ddcdac7caded15c6de1e4c4a0b0865bba074887
bnj-dez <designebenjamin@gmail.com>
2021-03-20 22:42:51 +0100

    Ajout des props et du typage au propriété contact

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import Contact from "./components/Contact";
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Contact
+          name="Elizabeth Mcdonalid"
+          avatar="https://randomuser.me/api/portraits/women/32.jpg"
+          status
+      />
+      <Contact
+          name="Louis Caldwell"
+          avatar="https://randomuser.me/api/portraits/men/24.jpg"
+          status={false}
+      />
+      <Contact
+          name="Hannah Hernandez"
+          avatar="https://randomuser.me/api/portraits/women/62.jpg"
+          status
+      />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import './Contact.css';
+import PropTypes from 'prop-types';
 
-function Contact() {
-    const name = "Elizabeth Mcdonalid";
-    const avatar = "https://randomuser.me/api/portraits/women/32.jpg";
-    const status = true;
-
+function Contact(props) {
     return (
         <div className="Contact">
-            <img className="avatar" src={avatar} alt="Image de profil"/>
+            <img className="avatar" src={props.avatar} alt="Image de profil"/>
             <div>
-                <h4 className="name">{name}</h4>
+                <h4 className="name">{props.name}</h4>
                 <div className="status">
-                    <div className={status ? "status-online" : "status-offline"} />
-                    <p className="status-text">{status ? "Online" : "Offline"}</p>
+                    <div className={props.status ? "status-online" : "status-offline"} />
+                    <p className="status-text">{props.status ? "Online" : "Offline"}</p>
                 </div>
             </div>
         </div>
     )
 }
+
+Contact.propTypes = {
+    name: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    status: PropTypes.bool.isRequired,
+};
 
 export default Contact;
