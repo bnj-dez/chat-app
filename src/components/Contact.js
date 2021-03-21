0e1f01363834199870f96d9531405ecd86eb693e
bnj-dez <designebenjamin@gmail.com>
2021-03-21 18:35:29 +0100

    Passage de Contact en class + Statut dynamique

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,19 +2,34 @@ import React from 'react';
 import './Contact.css';
 import PropTypes from 'prop-types';
 
-function Contact(props) {
-    return (
-        <div className="Contact">
-            <img className="avatar" src={props.avatar} alt="Image de profil"/>
-            <div>
-                <h4 className="name">{props.name}</h4>
-                <div className="status">
-                    <div className={props.status ? "status-online" : "status-offline"} />
-                    <p className="status-text">{props.status ? "Online" : "Offline"}</p>
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            online: this.props.status,
+        }
+    }
+
+    render() {
+        return (
+            <div className="Contact">
+                <img className="avatar" src={this.props.avatar} alt="avatar"/>
+                <div>
+                    <h4 className="name">{this.props.name}</h4>
+                    <div
+                        className="status"
+                        onClick={() => {
+                            const newOnline = !this.state.online;
+                            this.setState({online: newOnline})
+                        }}
+                    >
+                        <div className={this.state.online ? "status-online" : "status-offline"}/>
+                        <p className="status-text">{this.state.online ? "Online" : "Offline"}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 Contact.propTypes = {
