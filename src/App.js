9b6e388c9f51c282e7658cd43d4569fadadd2341
bnj-dez <designebenjamin@gmail.com>
2021-03-21 09:29:16 +0100

    Création du composant ContactList

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import Contact from "./components/Contact";
+import ContactList from "./components/ContactList";
 
 function App() {
   return (
     <div className="App">
-      <Contact
-          name="Elizabeth Mcdonalid"
-          avatar="https://randomuser.me/api/portraits/women/32.jpg"
-          status
-      />
-      <Contact
-          name="Louis Caldwell"
-          avatar="https://randomuser.me/api/portraits/men/24.jpg"
-          status={false}
-      />
-      <Contact
-          name="Hannah Hernandez"
-          avatar="https://randomuser.me/api/portraits/women/62.jpg"
-          status
-      />
+      <ContactList />
     </div>
   );
 }
---
+++ src/components/ContactList.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Contact from "./Contact";
+
+const users = [
+    {
+        name: 'Robert Reyes',
+        avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
+        online: false
+    },
+    {
+        name: 'Nellie Caldwell',
+        avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
+        online: true
+    },
+    {
+        name: 'Vernon Mason',
+        avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
+        online: true
+    },
+    {
+        name: 'Erica Hunt',
+        avatar: 'https://randomuser.me/api/portraits/women/87.jpg',
+        online: false
+    },
+    {
+        name: 'Juanita Phillips',
+        avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
+        online: true
+    }
+];
+
+const ContactList = () => (
+    <div>
+        {users.map(user => (
+            <Contact key={user.name} name={user.name} avatar={user.avatar} status={user.online} />
+            )
+        )}
+    </div>
+)
+
+export default ContactList;
